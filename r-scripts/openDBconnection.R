5dd0e4648c4f982956ef613f12c69468173a151e
F. R <3razor16@gmail.com>
2015-03-24 12:51:22 +0100

    fix memory problems, improve code

--- r-scripts/openDBconnection.R
+++ r-scripts/openDBconnection.R
@@ -1,3 +1,5 @@
+# http://www.jason-french.com/blog/2014/07/03/using-r-with-mysql-databases/
+
 library(RMySQL);
 con <- dbConnect(RMySQL::MySQL(), dbname= "searchlogs", group = "searchlogs");
 summary(con);
--- src/bot_detection/BotDetection.java
+++ src/bot_detection/BotDetection.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
- 
+
 import javax.sql.DataSource;
 
 import jdbc.DataSourceFactory;
@@ -36,11 +36,13 @@ public class BotDetection {
 	private static final String HEADER = "userId" + "\t" + "sessionId" + "\t"
 			+ "score" + "\t" + "date" + "\t" + "timegap" + "\t" + "query";
 	private UserObject userObject;
-	
+
 	private DataSource dataSource;
 	private Connection dbConnection;
-    private Statement sqlStatement;
-    private ResultSet resultSet;
+	private Statement sqlStatement;
+	private ResultSet resultSet;
+	private boolean processFile = true;
+	private ArrayList<Integer> userIDs;
 
 	/**
 	 * @param args
@@ -52,11 +54,12 @@ public class BotDetection {
 	}
 
 	public BotDetection() throws IOException, SQLException {
-		
+
+		userIDs = new ArrayList<Integer>();
 		dataSource = DataSourceFactory.getMySQLDataSource();
 		dbConnection = dataSource.getConnection();
 		sqlStatement = dbConnection.createStatement();
-		
+
 		//inputFile = new File(INPUT_PATH);
 		outputFileResults = new File(OUTPUT_PATH);
 		outputFileBots = new File(OUTPUT_BOTS);
@@ -72,61 +75,87 @@ public class BotDetection {
 		bufferedWriterResults.close();
 		bufferedWriterBots.close();
 	}
-	
+
 	private void queryDB(String sqlQuery) throws SQLException, IOException {
-//		boolean firstLine = true;
+		//		boolean firstLine = true;
 		ArrayList<QueryObject> list = new ArrayList<QueryObject>();
 		boolean firstID = true;
 		int prevID = 0;
 		int currID = 0;
 		counter = 0;
-//		resultSet = sqlStatement.executeQuery("select * from user_ct_test_collection_01 Limit 100;");
-		resultSet = sqlStatement.executeQuery("select * from user_ct_test_collection_01;");
-//		resultSet.first();
-		appendBotsList(HEADER);
-		while (resultSet.next()) {
-			counter++;
-			int userId = resultSet.getInt(1);
-			int sessionId = resultSet.getInt(2);
-			Timestamp date = resultSet.getTimestamp(3);
-			int timeGap = resultSet.getInt(4);
-			long epoc = resultSet.getLong(5);
-			String query = resultSet.getString(6);
-			int itemRank = resultSet.getInt(7);
-			String clickUrl = resultSet.getString(8);
-			QueryObject queryObject = new QueryObject(userId, sessionId, date, timeGap, epoc, query, itemRank, clickUrl);
-			appendBotsList(queryObject.printContent());
-			
-			
-			prevID = currID;
-			currID = userId;
-			
-			if (prevID == currID) {
-				
-				list.add(queryObject);
-				//System.out.println("(79)" + list.size());
-			} else {
-				if (firstID) {
-					firstID = false;
-					list.add(queryObject);
-					continue;
-				}
-				appendBotsList("current ID: " + list.size());
-				System.out.println(("current ID: " + list.size()));
-				scanForBots(list);
-//				checkForBots(list);
-				list.clear();
-				list.add(queryObject);
-			}
+		resultSet = sqlStatement.executeQuery("select distinct userId from user_ct_test_collection_01;");
+		while(resultSet.next()) {
+			userIDs.add(resultSet.getInt(1));			
+		}
+		System.out.println("Total number of Users: " + userIDs.size());
+ 		int index = 0;
+		for(int i : userIDs) {
 
-			// String output = parseLine(input);
-			// writeLine(output);
+			resultSet = sqlStatement.executeQuery("select * from user_ct_test_collection_01 where userId = " + i + ";");
+			if (index >= 50) {
+				break;
+			}
+			index++;
+
+
+			//		resultSet = sqlStatement.executeQuery("select * from user_ct_test_collection_01 Limit 100000;");
+			//		resultSet = sqlStatement.executeQuery("select * from user_ct_test_collection_01;");
+			//		appendBotsList(HEADER);
+			if (processFile) {
+//				int index = 0;
+				//resultSet.first();
+				int row = 0;
+				while (resultSet.next()) {
+					//row = resultSet.getRow();
+//					System.out.println(row);
+					//counter++;
+					int userId = resultSet.getInt(1);
+					int sessionId = resultSet.getInt(2);
+					Timestamp date = resultSet.getTimestamp(3);
+					int timeGap = resultSet.getInt(4);
+					long epoc = resultSet.getLong(5);
+					String query = resultSet.getString(6);
+					int itemRank = resultSet.getInt(7);
+					String clickUrl = resultSet.getString(8);
+					QueryObject queryObject = new QueryObject(userId, sessionId, date, timeGap, epoc, query, itemRank, clickUrl);
+					//appendBotsList(queryObject.printContent());
+
+
+					prevID = currID;
+					currID = userId;
+
+					if (prevID == currID) {
+
+						list.add(queryObject);
+						//System.out.println("(79)" + list.size());
+					} else {
+						if (firstID) {
+							list.clear();
+							firstID = false;
+							list.add(queryObject);
+							continue;
+						}
+						appendBotsList(i + ": "+ list.size());
+//						System.out.println(("current ID: " + list.size()));
+						System.out.println(i + ": "+ list.size());
+						scanForBots(list);
+						//				checkForBots(list);
+						list.clear();
+						list.add(queryObject);
+					}
+
+					// String output = parseLine(input);
+					// writeLine(output);
+
+				}
+//				System.out.println("end of list");
+			}
 		}
 
 		resultSet.close();
 	}
+
 	
-	/*
 	private void readCSV(Scanner sc) throws IOException {
 		boolean firstLine = true;
 		boolean firstID = true;
@@ -137,7 +166,7 @@ public class BotDetection {
 		while (counter < 27) {
 			System.out.println(counter + " 1# " + sc.nextLine());
 			String[] input = sc.nextLine().split("\t");
-	
+
 			if (firstLine) {
 				firstLine = false;
 				appendBotsList(HEADER);
@@ -146,11 +175,11 @@ public class BotDetection {
 			}
 			//System.out.println("prevID " + prevID + " currID " + currID);
 			counter++;
-	
+
 			prevID = currID;
 			currID = Integer.parseInt(input[0]);
 			if (prevID == currID) {
-				
+
 				list.add(input);
 				//System.out.println("(79)" + list.size());
 			} else {
@@ -164,28 +193,29 @@ public class BotDetection {
 				list.clear();
 				list.add(input);
 			}
-	
+
 			// String output = parseLine(input);
 			// writeLine(output);
 		}
-	
+
 		sc.close();
 	}
-	*/
+	 
 
 	private void scanForBots(ArrayList<QueryObject> list) {
 		userObject = new UserObject(list);
+		System.out.println(userObject.checkForFrequentQueries());
 		if(userObject.checkForFrequentQueries()) {
 			userObject.printUserObject();
 		}
 	}
 
-//	private void checkForBots(ArrayList<String[]> list) {
-//		userObject = new UserObject(list);
-//		if(userObject.checkForFrequentQueries()) {
-//			userObject.printUserObject();
-//		}
-//	}
+	private void checkForBots(ArrayList<String[]> list) {
+			userObject = new UserObject(list);
+			if(userObject.checkForFrequentQueries()) {
+				userObject.printUserObject();
+			}
+		}
 
 	private String parseLine(String[] in) throws IOException {
 		prevUserID = curUserID;
@@ -239,6 +269,11 @@ public class BotDetection {
 	private void appendBotsList(String output) throws IOException {
 		bufferedWriterBots.write(output);
 		bufferedWriterBots.newLine();
+		bufferedWriterBots.flush();
+	}
+	
+	private void appendBotsList(ArrayList<QueryObject> output) {
+		
 	}
 
 }
